249dff29858a66296d20c3a6b524c1cb80ae8aff
kerolesmonsef <kerolesmonsef@gmail.com>
2022-05-08 20:38:23 +0200

    f

--- src/main/java/com/example/demospringint/models/User.java
+++ src/main/java/com/example/demospringint/models/User.java
@@ -2,6 +2,7 @@ package com.example.demospringint.models;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -12,5 +13,7 @@ import javax.persistence.Table;
 public class User {
     @Id
     private Long id;
+
+    @Column(name = "name")
     private String name;
 }
